9d88bf69f9a80bde5397a73f1b5b5013d9819406
Stefan Kashner <s.kash@yahoo.com>
2019-04-11 20:08:58 -0400

    added initial header

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Header(props) {
+  return (
+    <div class="page-header text-center">
+      <h1>MealAppeal</h1>
+    </div>
+  );
+}
+
+export default Header;
