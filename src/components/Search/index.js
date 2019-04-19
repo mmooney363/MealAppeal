dcab9bac5dfb5d11ef5d0f21e896bb5f2dc9b45d
mikek4 <mike.katz@outlook.com>
2019-04-19 13:30:18 -0400

    Added two search bars for Yelp.

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -5,39 +5,33 @@ import Dropdown from "react-bootstrap/Dropdown";
 const Search = () => {
   return (
     <MDBCol className="col-md-8">
-      <input className="form-control" type="text" placeholder="Search" aria-label="Search" />
       <div>
         <container className="row">
         <Dropdown>
-          <Dropdown.Toggle md="4" variant="success" id="dropdown-basic">
+          <Dropdown.Toggle md="4" variant="success" id="dropdown-basic" className = "Price">
             Price
           </Dropdown.Toggle>
           <Dropdown.Menu>
-            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+            <Dropdown.Item href="#/action-1">$</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">$$</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">$$$</Dropdown.Item>
+            <Dropdown.Item href="#/action-4">$$$$</Dropdown.Item>
+
           </Dropdown.Menu>
         </Dropdown>
         <Dropdown >
-          <Dropdown.Toggle md="4" variant="success" id="dropdown-basic">
+          <Dropdown.Toggle md="4" variant="success" id="dropdown-basic" className="Rating">
             Rating
           </Dropdown.Toggle>
           <Dropdown.Menu>
-            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+            <Dropdown.Item href="#/action-1">1 star</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">2 stars</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">3 stars</Dropdown.Item>
+            <Dropdown.Item href="#/action-4">4 stars</Dropdown.Item>
+            <Dropdown.Item href="#/action-5">5 stars</Dropdown.Item>
           </Dropdown.Menu>
         </Dropdown>
-        <Dropdown >
-          <Dropdown.Toggle md="4" variant="success" id="dropdown-basic">
-            Distance
-          </Dropdown.Toggle>
-         <Dropdown.Menu>
-            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
+        
         </container>
       </div>
     </MDBCol>
