5b76bc7e9ebfb2d1d4bb363a7727b37eb2cce0b2
Stefan Kashner <s.kash@yahoo.com>
2019-04-20 21:15:59 -0400

    Search page styling

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,40 +1,42 @@
 import React from "react";
-import { MDBCol } from "mdbreact";
 import Dropdown from "react-bootstrap/Dropdown";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const Search = () => {
   return (
-    <MDBCol className="col-md-8">
-      <div>
-        <container className="row">
-        <Dropdown>
-          <Dropdown.Toggle md="4" variant="success" id="dropdown-basic" className = "Price">
-            Price
+    <Col className="">
+        <Row className="justify-content-center">
+          <Col xs lg="4">
+            <Dropdown>
+              <Dropdown.Toggle variant="success" id="dropdown-basic" className="Price">
+                Price
           </Dropdown.Toggle>
-          <Dropdown.Menu>
-            <Dropdown.Item href="#/action-1">$</Dropdown.Item>
-            <Dropdown.Item href="#/action-2">$$</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">$$$</Dropdown.Item>
-            <Dropdown.Item href="#/action-4">$$$$</Dropdown.Item>
+              <Dropdown.Menu>
+                <Dropdown.Item href="#/action-1">$</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">$$</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">$$$</Dropdown.Item>
+                <Dropdown.Item href="#/action-4">$$$$</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </Col>
 
-          </Dropdown.Menu>
-        </Dropdown>
-        <Dropdown >
-          <Dropdown.Toggle md="4" variant="success" id="dropdown-basic" className="Rating">
-            Rating
+          <Col xs lg="4">
+            <Dropdown>
+              <Dropdown.Toggle variant="success" id="dropdown-basic" className="Rating">
+                Rating
           </Dropdown.Toggle>
-          <Dropdown.Menu>
-            <Dropdown.Item href="#/action-1">1 star</Dropdown.Item>
-            <Dropdown.Item href="#/action-2">2 stars</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">3 stars</Dropdown.Item>
-            <Dropdown.Item href="#/action-4">4 stars</Dropdown.Item>
-            <Dropdown.Item href="#/action-5">5 stars</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-        
-        </container>
-      </div>
-    </MDBCol>
+              <Dropdown.Menu>
+                <Dropdown.Item href="#/action-1">1 star</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">2 stars</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">3 stars</Dropdown.Item>
+                <Dropdown.Item href="#/action-4">4 stars</Dropdown.Item>
+                <Dropdown.Item href="#/action-5">5 stars</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </Col>
+        </Row>      
+    </Col>
   );
 }
 
