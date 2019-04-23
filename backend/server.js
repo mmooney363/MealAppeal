0780623ecd4259771802defdd6c00a5e33be5bc4
mmooney363 <38362445+mmooney363@users.noreply.github.com>
2019-04-23 19:12:25 -0400

    Quickstuff (#26)
    * deleting boilerplate, bye bye pupster
    
    * mongo Schema
    
    * routes
    
    * mongo routes

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const UserRoutes = express.Router();
+const User = require('User');
 const PORT = 4000;
 const UserRoutes = express.Router();
 const User = require('User');
@@ -28,6 +30,37 @@ UserRoutes.route('/:id').get(function(req, res) {
         res.json(user);
     });
 });
+
+
+UserRoutes.route('/add').post(function(req, res) {
+    let user = new User(req.body);
+    user.save()
+        .then(user => {
+            res.status(200).json({'user': 'user added successfully'});
+        })
+        .catch(err => {
+            res.status(400).send('adding new user failed');
+        });
+});
+
+UserRoutes.route('/update/:id').post(function(req, res) {
+    User.findById(req.params.id, function(err, user) {
+        if (!user)
+            res.status(404).send("data is not found");
+        else
+            user.user_name = req.body.user_name;
+            user.user_password = req.body.user_password;
+            
+
+            user.save().then(user => {
+                res.json('User updated!');
+            })
+            .catch(err => {
+                res.status(400).send("Update not possible");
+            });
+    });
+});
+
 
 mongoose.connect('mongodb://127.0.0.1:27017/Users', { useNewUrlParser: true });
 const connection = mongoose.connection;
