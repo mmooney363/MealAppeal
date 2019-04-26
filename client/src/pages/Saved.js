37e70e43fa400073147c023e4e336dc6b723627d
Kash-6 <44305211+Kash-6@users.noreply.github.com>
2019-04-25 21:22:20 -0400

    passed id from restaurants to results (#41)

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,5 +1,4 @@
 import React from "react";
-import API from "../utils/API";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Container from "../components/Container";
