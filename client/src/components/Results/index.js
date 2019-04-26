fa54cfc367cd1bbfc10e589596693e5071928e51
Stefan Kashner <s.kash@yahoo.com>
2019-04-26 19:51:40 -0400

    added access to data in results from restaurant list

--- client/src/components/Results/index.js
+++ client/src/components/Results/index.js
@@ -16,29 +16,30 @@ class Results extends Component {
   }
 
   componentDidMount() {
-    
-    this.getRestaurants();
-   console.log(this.props.state.id)
+
+    // this.getRestaurants();
+    console.log("results component")
+    console.log(this.props.state.state.id)
   }
 
-  getRestaurants = () => {
-    this.setState({ loading: true })
+  //   getRestaurants = () => {
+  //     this.setState({ loading: true })
 
-    axios.get(`${'https://cors-anywhere.herokuapp.com/'}https://api.yelp.com/v3/businesses/matches?yelp_business_id=${this.props.state.id}`, {
-        headers: {
-            Authorization: `Bearer ${'jfKBdYwg2HcVvonXCl5dzGyumNbjmyezKnEVZ17MLE3mqWJX5UG__z0Zz9darrUiD8Eb8j6a0vg90MJuBZC58wRwOfbm_BzSXD0Q7_j4DaNzlFsHWovPzN_TihutXHYx'}`
-        },
-        params: {
-            limit: 1
-        }
-    }).then((res) => {
-            console.log(res.data.businesses)
-            this.setState({ results: res.data.businesses })
-        })
-        .catch((err) => {
-            this.setState({ errorState: "Sorry" })
-        })
-}
+  //     axios.get(`${'https://cors-anywhere.herokuapp.com/'}https://api.yelp.com/v3/businesses/matches?yelp_business_id=${this.props.state.id}`, {
+  //         headers: {
+  //             Authorization: `Bearer ${'jfKBdYwg2HcVvonXCl5dzGyumNbjmyezKnEVZ17MLE3mqWJX5UG__z0Zz9darrUiD8Eb8j6a0vg90MJuBZC58wRwOfbm_BzSXD0Q7_j4DaNzlFsHWovPzN_TihutXHYx'}`
+  //         },
+  //         params: {
+  //             limit: 1
+  //         }
+  //     }).then((res) => {
+  //             console.log(res.data.businesses)
+  //             this.setState({ results: res.data.businesses })
+  //         })
+  //         .catch((err) => {
+  //             this.setState({ errorState: "Sorry" })
+  //         })
+  // }
 
   render() {
     return (
@@ -46,25 +47,39 @@ class Results extends Component {
         <Card className="resultsCard">
           <Card.Header>
             <Accordion.Toggle as={Button} variant="link" eventKey="1">
-              Reviews picture
-     </Accordion.Toggle>
+            <img style={{
+                                margin: "0",
+                                height: "125px",
+                                width: "125px",
+                                position: "absolute",
+                                top: "50%",
+                                left: "50%",
+                                msTransform: "translate(-50%, -50%)",
+                                transform: "translate(-50%, -50%)"
+                            }}
+                                src={this.props.state.state.id.image_url} alt="" className="RestaurantInfo__img" />
+            </Accordion.Toggle>
+            <br></br>
+            <Accordion.Toggle as={Button} variant="link" eventKey="1">
+              {this.props.state.state.id.name}
+            </Accordion.Toggle>
           </Card.Header>
         </Card>
         <Card className="resultsCard">
           <Card.Header>
             <Accordion.Toggle as={Button} variant="link" eventKey="1">
               <i className="fas fa-user-edit"></i> Reviews
-     </Accordion.Toggle>
+            </Accordion.Toggle>
           </Card.Header>
         </Card>
         <Card className="resultsCard">
           {/* <Card.Header> */}
           <Accordion.Toggle as={Card.Header} variant="link" eventKey="0">
             <i className="fas fa-phone-volume"></i>  Call
-     </Accordion.Toggle>
+          </Accordion.Toggle>
           {/* </Card.Header> */}
           <Accordion.Collapse eventKey="0">
-            <Card.Body>867-5309</Card.Body>
+            <Card.Body>{this.props.state.state.id.phone}</Card.Body>
           </Accordion.Collapse>
         </Card>
         <Card className="resultsCard" >
@@ -72,14 +87,14 @@ class Results extends Component {
           <Card.Header>
             <Accordion.Toggle as={Button} variant="link" eventKey="1">
               <i className="fas fa-map-signs"></i>  Directions
-     </Accordion.Toggle>
+            </Accordion.Toggle>
           </Card.Header>
         </Card>
         <Card className="resultsCard">
           <Card.Header>
             <Accordion.Toggle as={Button} variant="link" eventKey="1">
               <i className="fas fa-laptop"></i>   Website
-     </Accordion.Toggle>
+            </Accordion.Toggle>
           </Card.Header>
         </Card>
       </Accordion>
