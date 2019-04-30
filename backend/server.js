cd8ef2486ead20ca102388f1d5469a8111345928
mikek4 <mike.katz@outlook.com>
2019-04-29 20:04:01 -0400

    Tweaked server.js (#66)

--- backend/server.js
+++ backend/server.js
@@ -13,11 +13,11 @@ app.use("/Users", UserRoutes);
 app.use(cors());
 app.use(bodyParser.json());
 
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static("/client/build"));
-}
+app.use(express.static("/client/build"));
 
-
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"))
+ });
 //app.use(routes);
 
 
@@ -28,11 +28,11 @@ const db = require("./config/keys").mongoURI;
 mongoose.connect(db, { useNewUrlParser: true });
 const connection = mongoose.connection;
 
-connection.once("open", function() {
+connection.once("open", function () {
   console.log("MongoDB database connection established successfully");
 });
 
-app.listen(process.env.PORT || PORT, function() {
+app.listen(process.env.PORT || PORT, function () {
   console.log("Server is running on Port: " + PORT);
 });
 
