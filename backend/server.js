eb44307bb2462a835390065664d3b7d217084e6d
mikek4 <mike.katz@outlook.com>
2019-04-30 21:17:14 -0400

    heroku tweaking

--- backend/server.js
+++ backend/server.js
@@ -23,11 +23,11 @@ app.use(
 app.use(bodyParser.json());
 
 if (process.env.NODE.ENV === "production") {
-  app.use(express.static("/client/build"));
+  app.use(express.static("client/build"));
 }
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"))
+  res.sendFile(path.join(__dirname, "./client/build/index.html"))
 });
 
 // DB Config
